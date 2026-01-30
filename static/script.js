a238047c60e9918539baa03b668cc16fc5be09dd
Abdul Rafay <abdulrafay88243@gmail.com>
2026-01-30 22:24:50 +0500

    spelling mistake

--- static/script.js
+++ static/script.js
@@ -175,7 +175,7 @@ setInterval(async () => {
             const m = new mapboxgl.Marker(div)
                 .setLngLat([lng, lat])
                 .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(
-                    `<strong style="color:black">⚠️ DISTRESS SIGNAL</strong><br>
+                    `<strong style="color:black">DISTRESS SIGNAL</strong><br>
                      <span style="color:black">${sig.time}</span>`
                 ))
                 .addTo(map);
